871a7c480d7d08b570adbaabc6fde5601660813c
JeevithaR3 <jeevithajr2005@gmail.com>
2026-02-28 00:51:45 +0530

    autoscaling

--- app.py
+++ app.py
@@ -22,6 +22,10 @@ app = Flask(__name__)
 def home():
     app_name = os.getenv("APP_NAME", "Default App")
     return f"{app_name} 🚀 Running on {socket.gethostname()}"
+@app.route("/health")
+def health():
+    return "OK", 200
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
